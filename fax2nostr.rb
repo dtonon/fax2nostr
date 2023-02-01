cdbcda06ff7a1eaf4666a4309ec3d4a44563ac24
Daniele Tonon <tonon@vitamino.it>
2023-02-01 18:40:32 +0100

    Add void.cat fallback if nostr.build is down

--- fax2nostr.rb
+++ fax2nostr.rb
@@ -28,6 +28,7 @@ Mail.defaults do
 end
 
 unread_emails = Mail.find(keys: ['NOT','SEEN'])
+puts "Found #{unread_emails.count} faxes!"
 unread_emails.each do |email|
 
   if email.multipart?
@@ -41,28 +42,61 @@ unread_emails.each do |email|
     pdf[0].border!(0, 0, 'white')
     pdf[0].alpha Magick::DeactivateAlphaChannel
     pdf[0].write(image_out)
+    
+    this_fax = nil
 
-    boundary = SecureRandom.hex(10)
-    uri = URI.parse("http://nostr.build/upload.php")
-    post_body = []
-    post_body << "--#{boundary}\r\n"
-    post_body << "Content-Disposition: form-data; name='fileToUpload'; filename='#{File.basename(image_out)}'\r\n"
-    post_body << "Content-Type: text/plain\r\n"
-    post_body << "\r\n"
-    post_body << File.read(image_out)
-    post_body << "\r\n--#{boundary}--\r\n"
-    
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request.body = post_body.join
-    request["Content-Type"] = "multipart/form-data, boundary=#{boundary}"
-    response = http.request(request)
-    
-    match = response.body.match(/(https:\/\/nostr\.build\/i\/nostr\.build_.+)<\/b>/)
-    if match
-      puts "Found => #{match[1]}"
-      faxes << match[1]
+    begin
+      nostr_build_url = "http://nostr.build/upload.php"
+      response = Net::HTTP.get_response(URI(nostr_build_url))
+      if response.code.to_i >= 200 && response.code.to_i < 300
+        boundary = SecureRandom.hex(10)
+        uri = URI.parse("http://nostr.build/upload.php")
+        post_body = []
+        post_body << "--#{boundary}\r\n"
+        post_body << "Content-Disposition: form-data; name='fileToUpload'; filename='#{File.basename(image_out)}'\r\n"
+        post_body << "Content-Type: text/plain\r\n"
+        post_body << "\r\n"
+        post_body << File.read(image_out)
+        post_body << "\r\n--#{boundary}--\r\n"
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.body = post_body.join
+        request["Content-Type"] = "multipart/form-data, boundary=#{boundary}"
+        response = http.request(request)
+        match = response.body.match(/(https:\/\/nostr\.build\/i\/nostr\.build_.+)<\/b>/)
+        this_fax = match[1]
+        puts "Found => #{this_fax}"
+      end
+    rescue => e
+      puts "#{nostr_build_url} => Error: #{e}"
     end
+
+    faxes << this_fax && next if this_fax
+
+    begin
+      void_cat_url = "https://void.cat/upload?cli=true"
+      response = Net::HTTP.get_response(URI(void_cat_url))
+      if response.code.to_i >= 200 && response.code.to_i < 300
+        uri = URI.parse(void_cat_url)
+        http = Net::HTTP.new(uri.host, uri.port, use_ssl: true)
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request["v-Filename"] = File.basename(image_out)
+        request["v-Content-Type"] = "image/png"
+        request.body = File.read(image_out)
+        response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+          http.request(request)
+        end
+        this_fax = response.body
+        puts "Found => #{this_fax}"
+      end
+    rescue => e
+      puts "#{void_cat_url} => Error: #{e}"
+    end
+
+    faxes << this_fax && next if this_fax
+
+    raise "Upload services are down!"
+
   end
 end
 
@@ -72,7 +106,7 @@ n = Nostr.new(private_key: nostr_prvkey)
 faxes.each do |fax|
 
   content = fax
-  content << "\n\nPowered by fax2nostr\nSend a fax to #{fax_number} to publish it on Nostr\nFollow the feed: @#{nostr_npub}"
+  content << "\n\nPowered by fax2nostr\nSend a fax to #{fax_number} to publish it on Nostr"
   event = n.build_note_event(content)
   puts event.inspect
 
